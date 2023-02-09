31035a09a7367796330d06cedb78cdf81d1f64cd
Yassine Rassy <yassinDevox>
2023-02-09 23:36:36 +0100

    add navigation btw home & posts pages

---
+++ nextjs-blog/pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function FirstPost () {
+  return (
+    <div>
+      <h1>First Post 😅</h1>
+      <h2>
+        <Link href='/'> Back to home page 😸 </Link>
+      </h2>
+    </div>
+  )
+}
