f0cafdf14a985a5e3e17d6f3f27b106898b2ea5f
Yassine Rassy <yassinDevox>
2023-02-10 23:42:02 +0100

    add profil image

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,9 +1,16 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 export default function FirstPost () {
   return (
     <div>
       <h1>First Post 😅</h1>
+      <Image
+        src='/images/profile.jpg' // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt='Your Name'
+      />
       <h2>
         <Link href='/'> Back to home page 😸 </Link>
       </h2>
