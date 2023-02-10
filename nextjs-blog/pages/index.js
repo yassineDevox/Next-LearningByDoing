2b1fc283c4995e7f228dbf043992288ccaf976e9
Yassine Rassy <yassinDevox>
2023-02-11 00:22:57 +0100

    loading bootstrap as at hirdparty scripts with Script component

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -16,16 +16,6 @@ export default function Home () {
         </h1>
       </main>
 
-      <footer>
-        <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Powered by{' '}
-          <img src='/vercel.svg' alt='Vercel' className={styles.logo} />
-        </a>
-      </footer>
 
       <style jsx>{`
         main {
@@ -36,24 +26,7 @@ export default function Home () {
           justify-content: center;
           align-items: center;
         }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
+       
         code {
           background: #fafafa;
           border-radius: 5px;
--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,9 +1,22 @@
+import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
+import Script from 'next/script'
 
 export default function FirstPost () {
   return (
-    <div>
+    <>
+      <Head>
+        <title>First Post </title>
+        
+      </Head>
+      <Script
+          src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN'
+          strategy='lazyOnload'
+          onLoad={() =>
+            console.log(`script loaded correctly, window.FB has been populated`)
+          }
+        />
       <h1>First Post 😅</h1>
       <Image
         src='/images/profile.jpg' // Route of the image file
@@ -14,6 +27,6 @@ export default function FirstPost () {
       <h2>
         <Link href='/'> Back to home page 😸 </Link>
       </h2>
-    </div>
+    </>
   )
 }
